857cd181cec8e3e09606e0924180b1a8af1caa9b
Anh Pham <phamquocanh2580@gmail.com>
2026-01-19 16:30:27 -0500

    Initial commit

---
+++ Source/SFHost/SFHost.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SFHost.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SFHost, "SFHost" );
